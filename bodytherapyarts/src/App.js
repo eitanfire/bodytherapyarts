53210d3c045e2d492b402151ae37a08da9791c3d
Eitan Fire <eitanfire@gmail.com>
2023-08-07 00:15:56 -0600

    creater Header

--- bodytherapyarts/src/App.js
+++ bodytherapyarts/src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
-import { Container, Navbar, NavbarBrand } from "reactstrap";
-import btalogo from "./app/assets/img/bta-logo.jpg";
+import Header from "./components/Header";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <Navbar
-        className="site-header"
-        dark
-        // color="primary"
-        sticky="top"
-        // expand="md"
-      >
-        <Container>
-          <NavbarBrand href="/">
-            <img
-              src={btalogo}
-              className="title-area"
-              alt="Body Therapy Arts l"
-            />
-          </NavbarBrand>
-        </Container>
-      </Navbar>{" "}
+      <Header />
     </div>
   );
 }
---
+++ bodytherapyarts/src/components/Header.js
@@ -0,0 +1,26 @@
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import btalogo from "../app/assets/img/bta-logo.jpg";
+
+const Header = () => {
+    return (
+ <Navbar
+        className="site-header"
+        dark
+        // color="primary"
+        sticky="top"
+        // expand="md"
+      >
+        <Container>
+          <NavbarBrand href="/">
+            <img
+              src={btalogo}
+              className="title-area"
+              alt="Body Therapy Arts l"
+            />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default Header;
