99e0f51d5e7ad687c90179ffc79dd87974fbefa1
Eitan Fire <eitanfire@gmail.com>
2023-08-10 13:41:07 -0600

    add avatar images for practioners

--- bodytherapyarts/src/app/shared/PRACTIONERS.js
+++ bodytherapyarts/src/app/shared/PRACTIONERS.js
@@ -1,7 +1,15 @@
+import Alyssa from "../assets/img/Alyssa.jpeg";
+import Cassandra from "../assets/img/Cassandra.png";
+import Wendell from "../assets/img/Wendell.jpeg";
+import Peggy from "../assets/img/Peggy.png";
+import Debbie from "../assets/img/Debbie.png";
+
+
 export const PRACTIONERS = [
   {
     id: 0,
     name: "Alyssa Rook",
+    image: Alyssa,
     specialty: "aliquid",
     about:
       "Animi ullam explicabo et eaque et excepturi laudantium maiores dolorum. Voluptatem dolorem ex. Rem in optio dolor consequatur. Et ipsa beatae. Est natus ut quia et quis rerum totam doloremque fuga. Aut ea ipsa modi et sit rerum sit.",
@@ -13,6 +21,7 @@ export const PRACTIONERS = [
   {
     id: 1,
     name: "Cassandra Schmidt",
+    image: Cassandra,
     specialty: "dolores",
     about:
       "Iure tempore quod. Nesciunt quam aut. Cumque dolorem iusto. Qui omnis cumque. Explicabo eaque tenetur omnis dolore. Consequatur voluptas est.",
@@ -24,6 +33,7 @@ export const PRACTIONERS = [
   {
     id: 2,
     name: "Peggy Bradtke",
+    image: Peggy,
     specialty: "delectus",
     about:
       "Dolorem expedita nemo optio voluptas aut dicta. Qui qui assumenda consequatur dolor quo ut. Modi qui rerum eos. Ut aliquid quis consectetur eos aut maiores.",
@@ -35,6 +45,7 @@ export const PRACTIONERS = [
   {
     id: 3,
     name: "Debbie Schamberger",
+    image: Debbie,
     specialty: "rerum",
     about:
       "Numquam ducimus quia. Est et possimus fugit est omnis necessitatibus. Sunt sed natus maxime voluptate sint id libero minima. Quia illo consectetur nobis. Dolores cum rerum at consequuntur dolores quia. Necessitatibus autem exercitationem ad et nulla similique consectetur.",
@@ -46,6 +57,7 @@ export const PRACTIONERS = [
   {
     id: 4,
     name: "Wendell Bergstrom",
+    image: Wendell,
     specialty: "omnis",
     about:
       "In neque maiores corrupti voluptatum quis sit. Nesciunt est itaque. Optio natus est id sit eius nulla nostrum.",
